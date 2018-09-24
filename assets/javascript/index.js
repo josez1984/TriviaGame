7bc78a2ff388edf59580eb9e9ceca86d37320e8b
root <jose.zapata305@gmail.com>
2018-09-24 03:44:00 -0400

    Update footer text

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -159,6 +159,10 @@ function nextQuestion(app) {
                 app.newQuestion();
                 $('html, body').animate({scrollTop: $(document).height()}, 'slow');
                 
+                $("#correct-answers-text").text("Correct: " + app.players[0].correct);
+    $("#wrong-answers-text").text("Wrong: " + app.players[0].wrong);
+                $("#footer-text").text("Correct: " + app.players[0].correct + ", " + "Wrong: " + app.players[0].wrong + ", Questions left: " + app.questionsLeft());
+
                 gameChoiceBtnClick(app);
                 $("#status-page").hide();
             } 
